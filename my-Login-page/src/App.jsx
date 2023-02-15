a16d1a45c06939021431df60a4a64604fca43a13
Tso4la1 <tsolajohn@gmail.com>
2023-02-15 05:44:20 +0100

    update App.css and App.js

--- my-Login-page/src/App.jsx
+++ my-Login-page/src/App.jsx
@@ -1,29 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Logo from './assets/logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div className="try"><img src={Logo} alt="{comopany-logo}" />
     <div className="App">
-      <img src="" alt="" />
-      <div>
-        <h1>Log in to your account</h1>
-        <div>
-          <p> Don't have an account? </p>
-          <span>Get Started</span>
-        </div>
-        <label htmlFor="email">Email</label>
-        <input type="email" />
-        <div>
-          <p>Password</p>
-          <p>Forget Password</p>
-          <input type="password" />
-        </div>
-        <button>Login</button>
+      
+      <div className='divHead'>
+        
+          <h1>Log in to your account</h1>
+          
+          <div className='can'>
+            <p> Don't have an account? </p>
+            <span>Get Started</span>
+          </div> 
+          <label htmlFor="email">Email</label>
+          <input type="email" />
+          
+            <p>Password</p>
+            <p>Forget Password</p>
+            <input type="password" />
+          
+          <button>Login</button>
+       
       </div>
-    </div>
+      </div>
+      </div>
   )
 }
 
